8bc54a2c9b6f13bb7477607647a08e44d1dcb824
Ritesh Vishwakarma <Ritesh@cgsadmins-iMac-2.local>
2017-12-15 19:34:44 +0530

    https bypass

--- app.js
+++ app.js
@@ -18,9 +18,28 @@
 // server.listen(port,()=>{console.log(`Server is running`)})
 const express = require('express');
 const app = express();
-
+const path = require('path');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
